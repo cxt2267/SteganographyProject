a5dfce5caf076f5e1a6828926e95223cf2d10820
Chelsea Takam <112098887+cxt2267@users.noreply.github.com>
2024-04-15 11:44:05 -0500

    Delete archive.py

--- archive.py
+++
@@ -1,7 +0,0 @@
-@bp.route('/login/<email>/<password>')
-def login(email, password):
-    user = DB.checkUser(email, password)
-    if user != "user not found":
-        return render_template('index.html', user = user)
-    else:
-        return render_template('login.html', user = user)
