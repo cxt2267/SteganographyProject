c6baeb4ea673fcf34ab946cca253d47144eb5aaf
Chelsea Takam <112098887+cxt2267@users.noreply.github.com>
2024-06-14 09:04:56 +0000

    cors

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from bp import bp
 import DB
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='./static')
 app.register_blueprint(bp)
+
+CORS(app)
 
 if __name__ == '__main__':  
    app.run()
