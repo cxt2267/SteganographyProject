7e7f90d3fb962e11c28cd543c19e19dccfc14544
Chelsea Takam <112098887+cxt2267@users.noreply.github.com>
2024-06-17 02:41:36 +0000

    test

--- DB.py
+++ DB.py
@@ -15,6 +15,7 @@ class DB:
         user_info = info
 
     def conn(self):
+        print(DB_INFO)
         con = mysql.connector.connect(
             host = DB_INFO["host"],
             user = DB_INFO["user"],
