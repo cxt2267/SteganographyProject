46707dfa34ce47a3d3cb2d36a0feb32426641742
Chelsea Takam <112098887+cxt2267@users.noreply.github.com>
2024-10-06 05:46:32 +0000

    update file storage

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,5 @@
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+//import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
 
 function checkUser() {
     if(localStorage.getItem("user") === null) {
@@ -90,4 +91,4 @@ function getPosts(user) {
         }); 
       })
     })
-  }
+}
